a73b6ce97b20115d6e2f7b4f231b00f446000ef3
sb.kang <sb.kang@q-sol.co.kr>
2021-11-01 17:18:51 +0900

    #5 퍼시스턴트 레이어: 스프링 데이터 JPA

---
+++ src/main/java/com/genug/todo/repository/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.genug.todo.repository;
+
+import com.genug.todo.model.entity.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    List<TodoEntity> findByUserId(String userId);
+}
