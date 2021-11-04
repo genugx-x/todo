1ac5d8ea73c8dd63e19184947b5de61d6d83e016
sb.kang <sb.kang@q-sol.co.kr>
2021-11-04 14:27:43 +0900

    #7 jjwt 디펜던시 추가 및 토큰 활용한 인증 기능 추가

---
+++ src/main/java/com/genug/todo/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.genug.todo.controller;
+
+import com.genug.todo.model.dto.ResponseDto;
+import com.genug.todo.model.dto.UserDto;
+import com.genug.todo.model.entity.UserEntity;
+import com.genug.todo.security.TokenProvider;
+import com.genug.todo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDto userDto) {
+        try {
+            UserEntity user = UserEntity.builder()
+                    .email(userDto.getEmail())
+                    .username(userDto.getUsername())
+                    .password(userDto.getPassword())
+                    .build();
+            UserEntity registeredUser = userService.create(user);
+            UserDto responseUserDto = UserDto.builder()
+                    .email(registeredUser.getEmail())
+                    .id(registeredUser.getId())
+                    .username(registeredUser.getUsername())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDto);
+        } catch (Exception e) {
+            ResponseDto responseDto = ResponseDto.builder().error(e.getMessage()).build();
+            return ResponseEntity
+                    .badRequest()
+                    .body(responseDto);
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDto userDto) {
+        UserEntity user = userService.getByCredentials(
+                userDto.getEmail(),
+                userDto.getPassword());
+        if (user != null) {
+            final String token = tokenProvider.create(user); // 토큰 생성
+            final UserDto responseUserDto = UserDto.builder()
+                    .email(user.getUsername())
+                    .id(user.getId())
+                    .token(token)
+                    .build();
+            return ResponseEntity.ok().body(responseUserDto);
+        } else {
+            ResponseDto responseDto = ResponseDto.builder()
+                    .error("Login failed.")
+                    .build();
+            return ResponseEntity
+                    .badRequest()
+                    .body(responseDto);
+        }
+    }
+
+}
---
+++ src/main/java/com/genug/todo/service/UserService.java
@@ -0,0 +1,32 @@
+package com.genug.todo.service;
+
+import com.genug.todo.model.entity.UserEntity;
+import com.genug.todo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserEntity create(final UserEntity userEntity){
+        if (userEntity == null || userEntity.getEmail() == null) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        final String email = userEntity.getEmail();
+        if (userRepository.existsByEmail(email)) {
+            log.warn("Email already exists {}", email);
+            throw new RuntimeException("Email already exists");
+        }
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity getByCredentials(final String email, final String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+
+}
